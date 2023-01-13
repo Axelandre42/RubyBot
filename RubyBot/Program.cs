8dad94ded01825262634d1622c0c2b07b103936c
Axelandre42 <5228298+Axelandre42@users.noreply.github.com>
2023-01-13 21:23:18 +0100

    Fix indents

--- RubyBot/Program.cs
+++ RubyBot/Program.cs
@@ -6,33 +6,34 @@ using RubyBot;
 using RubyBot.Model;
 
 var host = Host.CreateDefaultBuilder(args)
-    .ConfigureDiscordHost((context, config) =>
-    {
-        config.SocketConfig = new DiscordSocketConfig
-        {
-            LogLevel = LogSeverity.Verbose,
-            AlwaysDownloadUsers = true,
-            MessageCacheSize = 200,
-            UseInteractionSnowflakeDate = false,
-            GatewayIntents = GatewayIntents.AllUnprivileged
-                             - GatewayIntents.GuildScheduledEvents
-                             - GatewayIntents.GuildInvites
-        };
+	.ConfigureDiscordHost((context, config) =>
+	{
+		config.SocketConfig = new DiscordSocketConfig
+		{
+			LogLevel = LogSeverity.Verbose,
+			AlwaysDownloadUsers = true,
+			MessageCacheSize = 200,
+			UseInteractionSnowflakeDate = false,
+			GatewayIntents = GatewayIntents.AllUnprivileged
+			                 - GatewayIntents.GuildScheduledEvents
+			                 - GatewayIntents.GuildInvites
+		};
 
-        config.Token = context.Configuration["token"] ?? string.Empty;
-    })
-    .UseInteractionService((context, config) =>
-    {
-        config.LogLevel = LogSeverity.Info;
-        config.UseCompiledLambda = true;
-    })
-    .ConfigureServices((context, services) =>
-    {
-        var dbPath = Path.Join(Environment.CurrentDirectory, "ruby_bot.db");
-        services.AddDbContext<RolePlayContext>(b => b.UseLazyLoadingProxies()
-            .UseSqlite($"Data Source={dbPath}"));
-        services.AddHostedService<InteractionHandler>();
-    })
-    .Build();
+		config.Token = context.Configuration["token"] ?? string.Empty;
+	})
+	.UseInteractionService((context, config) =>
+	{
+		config.LogLevel = LogSeverity.Info;
+		config.UseCompiledLambda = true;
+	})
+	.ConfigureServices((context, services) =>
+	{
+		var dbPath = Path.Join(Environment.CurrentDirectory, "ruby_bot.db");
+		services.AddDbContext<RolePlayContext>(b => b
+			.UseLazyLoadingProxies()
+			.UseSqlite($"Data Source={dbPath}"));
+		services.AddHostedService<InteractionHandler>();
+	})
+	.Build();
 
 await host.RunAsync();
