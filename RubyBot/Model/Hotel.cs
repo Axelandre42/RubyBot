2423c543cc57b0f87b5f16a23fac8e42aa2b2e6c
Axelandre42 <5228298+Axelandre42@users.noreply.github.com>
2023-01-14 13:57:26 +0100

    Append schema with hotel features

---
+++ RubyBot/Model/Hotel.cs
@@ -0,0 +1,9 @@
+﻿namespace RubyBot.Model;
+
+public class Hotel
+{
+	public ulong Id { get; set; }
+	public byte Size { get; set; }
+	
+	public virtual List<Room> Rooms { get; set; } = null!;
+}
---
+++ RubyBot/Model/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace RubyBot.Model;
+
+public class Room
+{
+	public ulong Id { get; set; }
+	public byte Number { get; set; }
+	public bool Busy { get; set; }
+
+	public virtual Hotel Hotel { get; set; } = null!;
+}
